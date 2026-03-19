a7245e0271d0e47e82a06b5264f2bdb8e2fa03cb
Pawan1516 <pawakumar37835@gamil.com>
2026-03-20 04:09:52 +0530

    feat: Add PWA support with manifest and service worker; aesthetic refinements; ensure Twilio SMS hooks are active

---
+++ client/public/service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'the-turf-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Only intercept network requests meant for our app (not APIs, etc.)
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match('/index.html');
+      })
+    );
+  }
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
